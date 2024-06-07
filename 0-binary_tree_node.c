2a4430c2224ad8d8abab2bf7884491cebc39554b
Megz259 <125645532+Megz259@users.noreply.github.com>
2024-06-07 14:37:52 +0200

    Update 0-binary_tree_node.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -10,16 +10,16 @@
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node;
+	binary_tree_t *create_node;
 
-	new_node = malloc(sizeof(binary_tree_t));
-	if (!new_node)
+	create_node = malloc(sizeof(binary_tree_t));
+	if (!create_node)
 	{
 		return (NULL);
 	}
-	new_node->n = value;
-	new_node->parent = parent;
-	new_node->left = NULL;
-	new_node->right = NULL;
-	return (new_node);
+	create_node->n = value;
+	create_node->parent = parent;
+	create_node->left = NULL;
+	create_node->right = NULL;
+	return (create_node);
 }
