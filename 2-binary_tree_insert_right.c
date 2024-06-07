320f641998cfdd1d29b6d39eaec5ef67049373d6
Megz259 <125645532+Megz259@users.noreply.github.com>
2024-06-07 14:39:23 +0200

    Update 2-binary_tree_insert_right.c

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -10,21 +10,21 @@
 
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node;
+	binary_tree_t *create_node;
 	if (!parent)
 	{
 		return (NULL);
 	}
-	new_node = binary_tree_node(parent, value);
-	if (!new_node)
+	create_node = binary_tree_node(parent, value);
+	if (!create_node)
 	{
 		return (NULL);
 	}
 	if (parent->right != NULL)
 	{
-		new_node->right = parent->right;
-		parent->right->parent = new_node;
+		create_node->right = parent->right;
+		parent->right->parent = create_node;
 	}
-	parent->right = new_node;
-	return (new_node);
+	parent->right = create_node;
+	return (create_node);
 }
